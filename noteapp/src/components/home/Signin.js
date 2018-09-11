d341fd4f0972ce2d89d828f6cdf2330083646a6d
onHawk <luisd824@gmail.com>
2018-09-11 15:16:57 -0400

    styled sign in page

--- noteapp/src/components/home/Signin.js
+++ noteapp/src/components/home/Signin.js
@@ -7,25 +7,55 @@ import { login } from '../../actions/AuthActions';
 
 class Signin extends Component {
   handleFormSubmit = credentials => {
-    // const credentials = new FormData(event.target);
     this.props.login(credentials, this.props.history);
   };
   render() {
     const { handleSubmit } = this.props;
     return (
-      <div>
-        <h2>{this.props.error}</h2>
-        <form onSubmit={handleSubmit(this.handleFormSubmit)}>
-          <fieldset>
-            <label>Username:</label>
-            <Field name="username" component="input" type="text" />
-          </fieldset>
-          <fieldset>
-            <label>Password:</label>
-            <Field name="password" component="input" type="password" />
-          </fieldset>
-          <button action="submit">Sign in</button>
+      <div className="page-landing">
+        <h3>{this.props.error}</h3>
+        <div className="loginhead">
+          <h1 style={{ color: 'white' }}>Log in</h1>
+          <div>
+            <img src={require('./lambda.png')} />
+          </div>
+        </div>
+        <form className="signin" onSubmit={handleSubmit(this.handleFormSubmit)}>
+          <div className="signform">
+            <div className="inputs">
+              <fieldset className="fieldform" style={{ border: 'none' }}>
+                <label className="labels">Username:</label>
+                <Field
+                  className="fields"
+                  placeholder="username"
+                  name="username"
+                  component="input"
+                  type="text"
+                />
+              </fieldset>
+              <fieldset className="fieldform" style={{ border: 'none' }}>
+                <label className="labels">Password:</label>
+                <Field
+                  className="fields"
+                  placeholder="password"
+                  name="password"
+                  component="input"
+                  type="password"
+                />
+              </fieldset>
+            </div>
+            <div className="link" style={{ margin: '4rem 0 0 0' }}>
+              <button className="signer" action="submit">
+                Sign in
+              </button>
+            </div>
+          </div>
         </form>
+        <div>
+          <Link to="/signup" className="link">
+            <button className="signer">Sign Up</button>
+          </Link>
+        </div>
       </div>
     );
   }
