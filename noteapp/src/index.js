219296dc5e4061ca027cc6240fa1e4e7f73a0509
onHawk <luisd824@gmail.com>
2018-09-11 21:35:23 -0400

    redux tools

--- noteapp/src/index.js
+++ noteapp/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
 
@@ -9,7 +9,12 @@ import rootReducer from './reducers/index.js';
 
 import './index.css';
 
-const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(ReduxThunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
